03d605bd05a1ce11a53774acca7e1d58fdb166df
Nik <yawalkar.nikhil@gmail.com>
2023-02-03 23:32:03 +0530

    Initial Commit

---
+++ TheMovieDB/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TheMovieDB
+//
+//  Created by Nikhil yawalkar on 03/02/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TheMovieDB/TheMovieDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheMovieDBApp.swift
+//  TheMovieDB
+//
+//  Created by Nikhil yawalkar on 03/02/23.
+//
+
+import SwiftUI
+
+@main
+struct TheMovieDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
